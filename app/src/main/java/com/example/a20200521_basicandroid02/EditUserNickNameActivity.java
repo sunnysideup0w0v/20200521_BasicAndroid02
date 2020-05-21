5c323cca8c2f7af4da88b28df22b3246a9cd010b
toast eggs toast <59465490+sunnysideup0w0v@users.noreply.github.com>
2020-05-21 15:18:57 +0900

    닉네임 수정 화면 추가  - 그림 따로 그려야 함

---
+++ app/src/main/java/com/example/a20200521_basicandroid02/EditUserNickNameActivity.java
@@ -0,0 +1,26 @@
+package com.example.a20200521_basicandroid02;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class EditUserNickNameActivity extends BaseActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_edit_user_nick_name);
+        setupEvents();
+        setValues();
+    }
+
+    @Override
+    public void setupEvents() {
+
+    }
+
+    @Override
+    public void setValues() {
+
+    }
+}
